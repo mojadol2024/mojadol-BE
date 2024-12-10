77e735c20e7787dee3060c147030f38721defc62
변경태 <byeongyeongtae@byeongyeongtaeui-MacBookPro.local>
2024-12-10 16:33:39 +0900

    last dance

--- src/main/java/com/gnu/mojadol/repository/PhotoRepository.java
+++ src/main/java/com/gnu/mojadol/repository/PhotoRepository.java
@@ -13,8 +13,10 @@ import java.util.List;
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
     // 특정 게시글에 포함된 모든 사진을 가져 오는 메소드
     @Modifying
-    @Query("UPDATE Photo p SET p.deletedFlag = 1 WHERE p.board.boardSeq = :boardSeq")
+    @Query("delete from Photo p WHERE p.board.boardSeq = :boardSeq")
     void deleteFlagUpdate(@Param("boardSeq") int boardSeq);
+
+    void deleteByBoardBoardSeq(int boardSeq);
 
     @Query("SELECT p FROM Photo p WHERE p.board.boardSeq = :boardSeq AND p.deletedFlag = 0")
     List<Photo> findByBoard_BoardSeq(@Param("boardSeq") int boardSeq);
